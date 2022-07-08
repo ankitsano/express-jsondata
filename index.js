28b523ea5e667c6a37c68d3aee2159d8f2608b3c
ankit <ankitsanodiya789@gmail.com>
2022-07-08 15:23:45 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+// const fs =require('fs');
+// const express=require('express');
+// const app=express();
+
+// app.get('/student/getDetails',function(req,res){
+// fs.readFile('user.json','utf8',function(err,data){
+//     console.log(data);
+//     res.send(data);
+// })
+
+// })
+
+// app.listen(8000,function(){
+//     console.log("api working");
+// })
+
+
 const fs =require('fs');
 const express=require('express');
 const app=express();
 
-app.get('/student/getDetails',function(req,res){
-fs.readFile('user.json','utf8',function(err,data){
+app.get('/student/studentlist',function(req,res){
+fs.readFile('student.json','utf8',function(err,data){
     console.log(data);
     res.send(data);
 })
